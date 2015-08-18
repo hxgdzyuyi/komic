efb065c18852369c635cff4fae8579abb247f637
hxgdzyuyi <hxgdzyuyi@gmail.com>
2015-08-18 15:21:47 +0800

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Rever…"

--- src/views/reader/panel.js
+++ src/views/reader/panel.js
@@ -4,18 +4,9 @@ import { Link } from 'react-router'
 import app from 'app'
 
 export default class extends React.Component {
-  toggleThumbview() {
-    app.trigger('toggle:thumbview')
-  }
-
-  toggleConfigModal() {
-    app.trigger('toggle:modal', { modalId: 'config' })
-  }
-
   render() {
     var canvasModel = app.getModel('canvas')
       , currentPage = canvasModel.get('currentPage')
-
     return (
       <div className="aside">
         <ul className="panel">
@@ -23,12 +14,7 @@ export default class extends React.Component {
             <a href="#" title="页码" className="btn">{ currentPage }</a>
           </li>
           <li>
-            <a title="目录" className="btn" href="#"
-              onClick={ this.toggleThumbview }>总</a>
-          </li>
-          <li>
-            <a title="设定" className="btn" href="#"
-              onClick={ this.toggleConfigModal }>设</a>
+            <Link to="thumbview" title="目录" className="btn" params={{ page: currentPage }}>总</Link>
           </li>
           <li>
             <Link to="home" href="#" title="返回" className="btn">返</Link>
